b75dde09c534889b72e6264a92644522fd13240d
NielsHeijink <84321550+NielsHeijink@users.noreply.github.com>
2023-04-03 22:13:49 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-cd /CloudShirt/src/Web
-dotnet run --launch-profile Web
+#!/bin/bash               
+cd /CloudShirt/src/Web/bin/Debug/net6.0
+dotnet /CloudShirt/src/Web/bin/Debug/net6.0/Web.dll
+
+#cd /CloudShirt/src/Web
+#dotnet run --launch-profile Web
