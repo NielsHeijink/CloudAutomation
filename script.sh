2be3601c9269a0e8c3877be5f36bb87285214d7d
NielsHeijink <84321550+NielsHeijink@users.noreply.github.com>
2023-04-03 19:45:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-cd /CloudShirt/src/Web
-dotnet restore
-dotnet tool restore
-cd /CloudShirt/src/Web
-dotnet ef database update -c catalogcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
-dotnet ef database update -c appidentitydbcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
 dotnet run --launch-profile Web
